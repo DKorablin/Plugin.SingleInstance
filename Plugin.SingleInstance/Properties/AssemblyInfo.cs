3d88cc2b35dc1b09671cff71255c71f5004c1059
DKorablin2 <danila.korablin@unity3d.com>
2025-02-23 19:17:41 +0300

    Unifying plugins deployment

--- Plugin.SingleInstance/Properties/AssemblyInfo.cs
+++ Plugin.SingleInstance/Properties/AssemblyInfo.cs
@@ -1,7 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: ComVisible(false)]
 [assembly: Guid("0837f1bb-fb6b-42fe-8f4d-1ee0706caa47")]
 [assembly: System.CLSCompliant(true)]
 
@@ -9,14 +8,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyMetadata("ProjectUrl", "https://dkorablin.ru/project/Default.aspx?File=121")]
 #else
 
-[assembly: AssemblyTitle("Plugin.SingleInstance")]
 [assembly: AssemblyDescription("Limit application to single instance")]
-#if DEBUG
-[assembly: AssemblyConfiguration("Debug")]
-#else
-[assembly: AssemblyConfiguration("Release")]
-#endif
-[assembly: AssemblyCompany("Danila Korablin")]
-[assembly: AssemblyProduct("Plugin.SingleInstance")]
-[assembly: AssemblyCopyright("Copyright © Danila Korablin 2011-2024")]
+[assembly: AssemblyCopyright("Copyright © Danila Korablin 2011-2025")]
 #endif
